4c75fd2d5110f258fa66ddce0a33c2b4b346717f
stone <yshitouli@163.com>
2020-11-13 09:05:25 +0800

    更新了数据和路由

---
+++ src/components/InOutList.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h1>{{yearMonth}}</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "InOutList",
+	props: {
+		yearMonth: String
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/InAndOut.vue
+++ src/views/InAndOut.vue
@@ -2,29 +2,47 @@
 	<div class="about">
 		<h1>这里是收支页</h1>
 		<h2>{{this.$route.params}}</h2>
+		<h3>{{getDataList}}</h3>
+
+		<!-- <InOutList :yearMonth="yearMonth"></InOutList> -->
 	</div>
 </template>
 
 
 <script>
+// import InOutList from '../components/InOutList.vue';
+import { list } from '../data';
+import { util } from '../util';
 
 export default {
 	name: "InAndOut",
 	data() {
 		return {
-			yearMonth: {
-				year: Number(this.$route.params.year),
-				month: Number(this.$route.params.month)
-			}
+			year: null,
+			month: null,
+			dataList: []
+		}
+	},
+	components: {
+		// InOutList
+	},
+	computed: {
+		getDataList() {
+			console.log(this.year+"年"+this.month+"月：",util.getList(list,this.year,this.month))
+			return util.getList(list,this.year,this.month);
 		}
 	},
 	methods: {
 		getParams() {
-			console.log(this.yearMonth.year);
-			console.log(this.yearMonth.month);
+			this.year = Number(this.$route.params.year);
+			this.month = Number(this.$route.params.month);
 		}
 	},
+	watch: {},
 	created() {
+		this.getParams();
+	},
+	updated() {
 		this.getParams();
 	}
 }
