3a86a23b992671ac952ac6bda61defe2eababcfc
liyongee <liyongee@foxmail.com>
2020-12-10 09:51:51 +0800

    更新了数据

--- docs/js/about.b2ef3d55.js
+++ docs/js/about.b2ef3d55.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],o={name:"About",methods:{getD:function(){console.log(this.$route.params)}},created:function(){this.getD()}},u=o,c=n("c701"),i=Object(c["a"])(u,a,s,!1,null,null,null);e["default"]=i.exports}}]);
-//# sourceMappingURL=about.f70f3b93.js.map
+//# sourceMappingURL=about.b2ef3d55.js.map
